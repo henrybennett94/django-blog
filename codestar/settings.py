d91f432614496e8fe9830b310aed66976b7c8e16
Henry Bennet <hwbennett94@gmail.com>
2024-07-23 14:49:51 +0000

    Set debug to False for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-henrybennett-djangoblog-f7z0outmpra.ws.codeinstitute-ide.net', 
                 '.herokuapp.com']
